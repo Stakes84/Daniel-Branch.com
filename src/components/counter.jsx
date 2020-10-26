68270ff8d97e8b92af33d951fb6dfc34426f852a
Daniel Branch <stakes84@gmail.com>
2020-10-25 23:43:07 -0400

    f

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Daniel Branch</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
