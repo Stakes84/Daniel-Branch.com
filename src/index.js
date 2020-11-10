40b814189c0c4cae7bafda87d08b9a2b8b2e6425
Daniel Branch <stakes84@gmail.com>
2020-11-09 21:46:05 -0500

    cleaning up the files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDom from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
 import App from "./App";
 
-//ReactDom.render(<App />, document.getElementById("root"));
-//serviceWorker.unregister();
-
-ReactDom.render(<Counter />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
